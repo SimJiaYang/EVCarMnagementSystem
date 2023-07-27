769081f2bcce948239466868cebe59174eeffab7
Louis <7jia1421@gmail.com>
2023-07-27 18:38:44 +0800

    Initial commit

---
+++ src/Manufacturer.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class CarManufacturer {
+    private String carManufacturerID;
+    private String carManufacturerName;
+    private ArrayList<ElectricCar> carModels;
+}
